8efade22ff1501a3b2a8b77c7797cae244f51526
Moisés Medina <moidmedina@gmail.com>
2022-05-23 20:56:20 +0200

    feat: add range datepicker

--- src/app/assignment/selection-sheets-assignment/selection-sheets-assignment.component.ts
+++ src/app/assignment/selection-sheets-assignment/selection-sheets-assignment.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-selection-sheets-assignment',
-  templateUrl: './selection-sheets-assignment.component.html',
-  styleUrls: ['./selection-sheets-assignment.component.scss']
+  selector: "app-selection-sheets-assignment",
+  templateUrl: "./selection-sheets-assignment.component.html",
+  styleUrls: ["./selection-sheets-assignment.component.scss"],
 })
 export class SelectionSheetsAssignmentComponent implements OnInit {
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
