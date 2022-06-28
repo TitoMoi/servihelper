fe5e3a6dfa54418706065618a1698a1ffdabbd49
Moisés Medina <moidmedina@gmail.com>
2022-06-28 11:17:07 +0200

    chore: add sort json i18n

---
+++ scripts/sort-i18json.js
@@ -0,0 +1,10 @@
+const sortJson = require("sort-json");
+const fs = require("fs-extra");
+
+const i18nFileNames = fs.readdirSync("./src/assets/i18n/");
+console.log(i18nFileNames);
+const options = { ignoreCase: true };
+
+i18nFileNames.forEach((filename) => {
+  sortJson.overwrite("./src/assets/i18n/" + filename, options);
+});
