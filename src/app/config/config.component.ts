b2adaa011f983a4decab2676dec42b984b8d38f8
Moisés Medina <moidmedina@gmail.com>
2022-08-10 15:16:45 +0200

    chore: update 3.2.6

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -74,7 +74,7 @@ export class ConfigComponent {
 
   //Restart data
   config: ConfigInterface = {
-    appVersion: "3.2.5",
+    appVersion: "3.2.6",
     lang: "en",
     assignmentHeaderTitle: "",
     assignmentPrincipalTitle: "",
