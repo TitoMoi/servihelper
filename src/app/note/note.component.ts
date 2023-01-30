19bb978f37c5afc85429c7fa130be462ac6fdde2
Moisés Medina <moidmedina@gmail.com>
2023-01-30 16:27:11 +0100

    perf: notes remove table use grid

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,7 +1,7 @@
-import { NoteInterface, NoteTableInterface } from 'app/note/model/note.model';
-import { NoteService } from 'app/note/service/note.service';
+import { NoteInterface } from "app/note/model/note.model";
+import { NoteService } from "app/note/service/note.service";
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-note",
@@ -11,32 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class NoteComponent implements OnInit {
   notes: NoteInterface[];
 
-  //Table
-  displayedColumns: string[] = ["name", "editIcon", "deleteIcon"];
-
-  dataSource: NoteTableInterface[];
-
   constructor(private noteService: NoteService) {}
 
   ngOnInit(): void {
     this.notes = this.noteService.getNotes();
-    this.fillDataSource(this.notes);
-  }
-
-  trackByIdFn(index, note: NoteInterface) {
-    return note.id;
-  }
-
-  fillDataSource(notesPage: NoteInterface[]) {
-    const dataSourceTemp: NoteTableInterface[] = [];
-    for (const note of notesPage) {
-      //Populate datasource, values is in order
-      dataSourceTemp.push({
-        id: note.id,
-        name: note.name,
-      });
-    }
-    //Update the view
-    this.dataSource = dataSourceTemp;
   }
 }
