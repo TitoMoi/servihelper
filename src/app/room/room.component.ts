c4c1f0cc06ab0dc7ad25e3da36f1f7ca8a72b5f7
Moisés Medina <moidmedina@gmail.com>
2023-01-30 15:05:30 +0100

    perf: removed table of rooms and use grid

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -11,35 +11,15 @@ import { Component, OnInit } from "@angular/core";
 export class RoomComponent implements OnInit {
   rooms: RoomInterface[];
 
-  //Table
-  displayedColumns: string[] = ["name", "order", "editIcon", "deleteIcon"];
-
-  dataSource: RoomTableInterface[];
-
   constructor(private roomService: RoomService) {}
 
   ngOnInit(): void {
     this.rooms = this.roomService
       .getRooms()
       .sort((a, b) => (a.order > b.order ? 1 : -1));
-    this.fillDataSource(this.rooms);
   }
 
   trackByIdFn(index, room: RoomInterface) {
     return room.id;
-  }
-
-  fillDataSource(roomsPage: RoomInterface[]) {
-    const dataSourceTemp: RoomTableInterface[] = [];
-    for (const room of roomsPage) {
-      //Populate datasource, values is in order
-      dataSourceTemp.push({
-        id: room.id,
-        name: room.name,
-        order: room.order,
-      });
-    }
-    //Update the view
-    this.dataSource = dataSourceTemp;
   }
 }
