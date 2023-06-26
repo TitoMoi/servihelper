84f5ef41b25eeb98eed2d337265ec9ac689b5db6
Tito Moi <moidmedina@gmail.com>
2023-06-26 14:58:13 +0200

    chore: create artifacts

--- scripts/clean-release.js
+++ scripts/clean-release.js
@@ -1,3 +1,27 @@
 const fs = require("fs-extra");
+const path = require("path");
+const package = fs.readJsonSync("./package.json");
 
-fs.removeSync("./release");
+const releaseDir = "./release";
+
+fs.readdir(releaseDir, (err, files) => {
+  if (err) {
+    console.error(err);
+  }
+
+  files.forEach((file) => {
+    const fileDir = path.join(releaseDir, file);
+
+    //Generated files of Windows, Mac and Linux that we want to keep
+    if (file === "win-unpacked" || file.match(/.*\.(dmg|AppImage)$/)?.length > 0) {
+      console.log("found " + fileDir);
+
+      if (file === "win-unpacked") {
+        fs.renameSync(fileDir, path.join(releaseDir + `/servihelper-${package.version}`));
+      }
+    } else {
+      fs.removeSync(fileDir);
+      console.log("removed " + fileDir);
+    }
+  });
+});
