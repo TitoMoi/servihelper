e076e370bc62eabae6a6d110622718e1f509c0e0
Tito Moi <moidmedina@gmail.com>
2023-07-07 19:42:19 +0200

    styles: prettier

--- src/app/map/create-update-map/create-update-map.component.ts
+++ src/app/map/create-update-map/create-update-map.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component } from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 import { CommonModule, NgFor, NgIf } from "@angular/common";
-import {icon, Map, TileLayer, Polygon, Marker} from "leaflet";
+import { icon, Map, TileLayer, Polygon, Marker } from "leaflet";
 import { Subscription, fromEvent } from "rxjs";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
@@ -10,17 +10,35 @@ import { MatTooltipModule } from "@angular/material/tooltip";
 @Component({
   selector: "app-create-update-map",
   standalone: true,
-  imports: [CommonModule,
+  imports: [
+    CommonModule,
     TranslocoModule,
     MatButtonModule,
     NgIf,
     NgFor,
     MatIconModule,
-  MatTooltipModule],
+    MatTooltipModule,
+  ],
   templateUrl: "./create-update-map.component.html",
   styleUrls: ["./create-update-map.component.scss"],
 })
-export class CreateUpdateMapComponent implements AfterViewInit {
+export class CreateUpdateMapComponent implements OnInit, AfterViewInit {
+  ngOnInit(): void {
+    const iconRetinaUrl = "assets/marker-icon-2x.png";
+    const iconUrl = "assets/marker-icon.png";
+    const shadowUrl = "assets/marker-shadow.png";
+    const iconDefault = icon({
+      iconRetinaUrl,
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41],
+    });
+    Marker.prototype.options.icon = iconDefault;
+  }
 
   subscription: Subscription = new Subscription();
   map: Map;
@@ -28,63 +46,47 @@ export class CreateUpdateMapComponent implements AfterViewInit {
   markerRef: Marker[] = [];
 
   polygon = [];
-  polygonRef:Polygon;
+  polygonRef: Polygon;
 
   ngAfterViewInit(): void {
-const iconRetinaUrl = 'assets/marker-icon-2x.png';
-const iconUrl = 'assets/marker-icon.png';
-const shadowUrl = 'assets/marker-shadow.png';
-const iconDefault = icon({
-  iconRetinaUrl,
-  iconUrl,
-  shadowUrl,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
-});
-Marker.prototype.options.icon = iconDefault;
     this.map = new Map("map").setView([51.505, -0.09], 13);
 
     new TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        minZoom: 3,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      maxZoom: 19,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map);
+
+    //https://leafletjs.com/reference.html#map-click
+    this.subscription.add(
+      fromEvent(this.map, "click").subscribe((clickEvent: any) => {
+        if (!this.polygonRef) {
+          this.polygon.push(clickEvent.latlng);
+          this.markerRef.push(new Marker(clickEvent.latlng).addTo(this.map));
+        }
       })
-      .addTo(this.map);
-
-      
-
-  //https://leafletjs.com/reference.html#map-click
-  this.subscription.add(fromEvent(this.map, 'click').subscribe((clickEvent: any) => {
-    if(!this.polygonRef) {
-      this.polygon.push(clickEvent.latlng);
-      this.markerRef.push(new Marker(clickEvent.latlng).addTo(this.map));
-    }
-  }));
+    );
   }
 
   isCreatePolygonBtnDisabled() {
-    return this.polygon.length < 2 || !!(this.polygonRef) === true
+    return this.polygon.length < 2 || !!this.polygonRef === true;
   }
 
   createPolygon() {
     this.polygonRef = new Polygon(this.polygon).addTo(this.map);
   }
-  removePolygon(){
+  removePolygon() {
     this.polygonRef.remove();
     this.polygon = [];
     this.polygonRef = undefined;
     this.removeMarkers();
   }
-  removeMarkers(){
-    this.markerRef.forEach(m => m.remove());
+  removeMarkers() {
+    this.markerRef.forEach((m) => m.remove());
     this.markerRef = [];
 
     //Prevent create a polygon when no markers are visible
-    if(!this.polygonRef) {
+    if (!this.polygonRef) {
       this.polygon = [];
     }
   }
