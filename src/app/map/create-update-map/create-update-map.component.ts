97a7c731e4692615c15b14f4a9ec85695b10f01b
Tito Moi <moidmedina@gmail.com>
2023-07-07 19:22:14 +0200

    feat: wip on maps

--- src/app/map/create-update-map/create-update-map.component.ts
+++ src/app/map/create-update-map/create-update-map.component.ts
@@ -1,24 +1,73 @@
-import { Component, OnInit } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import leaflet from "leaflet";
+import { AfterViewInit, Component } from "@angular/core";
+import { CommonModule, NgFor, NgIf } from "@angular/common";
+import {Map, TileLayer, Polygon, Marker} from "leaflet";
+import { Subscription, fromEvent } from "rxjs";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { TranslocoModule } from "@ngneat/transloco";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @Component({
   selector: "app-create-update-map",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    TranslocoModule,
+    MatButtonModule,
+    NgIf,
+    NgFor,
+    MatIconModule,
+  MatTooltipModule],
   templateUrl: "./create-update-map.component.html",
   styleUrls: ["./create-update-map.component.scss"],
 })
-export class CreateUpdateMapComponent implements OnInit {
-  ngOnInit(): void {
-    var map = leaflet.map("map").setView([51.505, -0.09], 13);
+export class CreateUpdateMapComponent implements AfterViewInit {
 
-    leaflet
-      .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  subscription: Subscription = new Subscription();
+  map: Map;
+
+  markerRef: Marker[] = [];
+
+  polygon = [];
+  polygonRef:Polygon;
+
+  ngAfterViewInit(): void {
+    this.map = new Map("map").setView([51.505, -0.09], 13);
+
+    new TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
+        minZoom: 3,
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       })
-      .addTo(map);
+      .addTo(this.map);
+
+      
+
+  //https://leafletjs.com/reference.html#map-click
+  this.subscription.add(fromEvent(this.map, 'click').subscribe((clickEvent: any) => {
+    if(!this.polygonRef) {
+      this.polygon.push(clickEvent.latlng);
+      this.markerRef.push(new Marker(clickEvent.latlng).addTo(this.map));
+    }
+  }));
+  }
+
+  createPolygon() {
+    this.polygonRef = new Polygon(this.polygon).addTo(this.map);
+  }
+  removePolygon(){
+    this.polygonRef.remove();
+    this.polygon = [];
+    this.polygonRef = undefined;
+    this.removeMarkers();
+  }
+  removeMarkers(){
+    this.markerRef.forEach(m => m.remove());
+    this.markerRef = [];
+
+    //Prevent create a polygon when no markers are visible
+    if(!this.polygonRef) {
+      this.polygon = [];
+    }
   }
 }
