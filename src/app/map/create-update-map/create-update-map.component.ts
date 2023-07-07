571c423326c91eb7f16fe91a0c4ac7bdae5222f5
Moisés Medina <moises.medina@ineo-it.es>
2023-07-07 14:35:59 +0200

    feat: wip added css and types for leaflet

--- src/app/map/create-update-map/create-update-map.component.ts
+++ src/app/map/create-update-map/create-update-map.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import leaflet from "leaflet";
 
 @Component({
-  selector: 'app-create-update-map',
+  selector: "app-create-update-map",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './create-update-map.component.html',
-  styleUrls: ['./create-update-map.component.scss']
+  templateUrl: "./create-update-map.component.html",
+  styleUrls: ["./create-update-map.component.scss"],
 })
-export class CreateUpdateMapComponent {
+export class CreateUpdateMapComponent implements OnInit {
+  ngOnInit(): void {
+    var map = leaflet.map("map").setView([51.505, -0.09], 13);
 
+    leaflet
+      .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      })
+      .addTo(map);
+  }
 }
