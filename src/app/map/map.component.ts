0c3d015f802643092a7bd9bcbd6ea96e0514a54e
Tito Moi <moidmedina@gmail.com>
2023-07-30 12:01:09 +0200

    fix: radio default nav

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,9 +1,18 @@
-import { Component } from "@angular/core";
-import { CommonModule, NgFor, NgIf } from "@angular/common";
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { TranslocoModule } from "@ngneat/transloco";
-import { MatRadioModule } from "@angular/material/radio";
+import { MatRadioGroup, MatRadioModule } from "@angular/material/radio";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-map",
@@ -14,12 +23,34 @@ import { MatRadioModule } from "@angular/material/radio";
     MatButtonModule,
     RouterLink,
     RouterLinkActive,
-    NgIf,
-    NgFor,
     MatRadioModule,
     RouterOutlet,
+    ReactiveFormsModule,
   ],
   templateUrl: "./map.component.html",
   styleUrls: ["./map.component.scss"],
 })
-export class MapComponent {}
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild("matRadioGroup") matRadioGroup: MatRadioGroup;
+  selectedRadioOptionControl = new FormControl();
+
+  subscription = new Subscription();
+  constructor(private cdr: ChangeDetectorRef, private router: Router) {}
+  ngOnInit(): void {
+    this.subscription.add(
+      this.selectedRadioOptionControl.valueChanges.subscribe((value) =>
+        value === "1"
+          ? this.router.navigate(["map/territory"])
+          : this.router.navigate(["map/territoryGroup"])
+      )
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.selectedRadioOptionControl.patchValue("1"); //default navigation
+    this.cdr.detectChanges();
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
