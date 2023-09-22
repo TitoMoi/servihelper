37f412ea35b5adbc8482ff3c4cb90539fd6dc96b
Tito Moi <moidmedina@gmail.com>
2023-09-22 20:48:27 +0200

    feat: wip on lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { ConfigService } from "app/config/service/config.service";
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
 import { PdfService } from "./services/pdf.service";
@@ -24,6 +24,12 @@ export class AppComponent implements OnInit {
   //Flag to render the app when the paths are resolved (online or offline)
   dataLoaded = false;
   showLockMsg = false;
+
+  //Register when we quit the app, works with the cross X
+  @HostListener("window:unload", ["$event"])
+  unloadHandler() {
+    this.lockService.releaseLock();
+  }
 
   constructor(
     private sharedService: SharedService,
