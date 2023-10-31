5a7f4b7f3ed5523746afe96e23eeb1a06acdb14e
Tito Moi <moidmedina@gmail.com>
2023-10-31 09:51:38 +0100

    chore: support russian

--- src/app/services/pdf.service.ts
+++ src/app/services/pdf.service.ts
@@ -118,7 +118,7 @@ export class PdfService {
    */
   checkLangExists(name: string) {
     if (this.S89 === name) {
-      const availableTemplates = ["en", "ca", "es", "fr", "pt", "it", "de", "nl", "ro"];
+      const availableTemplates = ["en", "ca", "es", "fr", "pt", "it", "de", "nl", "ro", "ru"];
       return availableTemplates.includes(this.configService.getConfig().lang);
     }
     return false;
