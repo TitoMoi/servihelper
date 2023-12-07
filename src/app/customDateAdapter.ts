09ddb93e85b7f7c8228aac20eeae6d55152b0dae
Tito Moi <moidmedina@gmail.com>
2023-12-07 14:56:24 +0100

    fix: remove deprecated arg

--- src/app/customDateAdapter.ts
+++ src/app/customDateAdapter.ts
@@ -1,6 +1,5 @@
 import { ConfigService } from "app/config/service/config.service";
 
-import { Platform } from "@angular/cdk/platform";
 import { Injectable } from "@angular/core";
 import { NativeDateAdapter } from "@angular/material/core";
 import { TranslocoService } from "@ngneat/transloco";
@@ -8,12 +7,8 @@ import { TranslocoService } from "@ngneat/transloco";
 /** Adapts the native JS Date for use with cdk-based components that work with dates. */
 @Injectable()
 export class CustomDateAdapter extends NativeDateAdapter {
-  constructor(
-    translocoService: TranslocoService,
-    platform: Platform,
-    private configService: ConfigService
-  ) {
-    super(translocoService.getActiveLang(), platform);
+  constructor(translocoService: TranslocoService, private configService: ConfigService) {
+    super(translocoService.getActiveLang());
   }
 
   override getFirstDayOfWeek(): number {
