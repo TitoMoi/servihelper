db4a26c2d36b311f1fc32c3c3a276c95d8a4e65a
Tito Moi <moidmedina@gmail.com>
2023-12-19 11:24:22 +0100

    perf: remove material from main

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,6 @@ import { importProvidersFrom } from "@angular/core";
 import { APP_CONFIG } from "./environments/environment";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
-import { LayoutModule } from "@angular/cdk/layout";
-import { NgxEditorModule } from "ngx-editor";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatChipsModule } from "@angular/material/chips";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatInputModule } from "@angular/material/input";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatSelectModule } from "@angular/material/select";
-import { MatListModule } from "@angular/material/list";
-import { MatIconModule } from "@angular/material/icon";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { ReactiveFormsModule } from "@angular/forms";
 import { withInterceptorsFromDi, provideHttpClient } from "@angular/common/http";
 import { provideTranslocoLocale } from "@ngneat/transloco-locale";
 import { routes } from "./app/app-routing";
@@ -91,27 +73,7 @@ bootstrapApplication(AppComponent, {
         pl: "pl-PL",
       },
     }),
-    importProvidersFrom(
-      ReactiveFormsModule,
-      MatToolbarModule,
-      MatSidenavModule,
-      MatButtonModule,
-      MatCardModule,
-      MatIconModule,
-      MatListModule,
-      MatSelectModule,
-      MatFormFieldModule,
-      MatInputModule,
-      MatCheckboxModule,
-      MatTooltipModule,
-      MatExpansionModule,
-      MatDatepickerModule,
-      MatNativeDateModule,
-      MatChipsModule,
-      MatDialogModule,
-      NgxEditorModule,
-      LayoutModule
-    ),
+    importProvidersFrom(MatNativeDateModule),
     { provide: DateAdapter, useClass: CustomDateAdapter },
   ],
 }).catch((err) => console.error(err));
