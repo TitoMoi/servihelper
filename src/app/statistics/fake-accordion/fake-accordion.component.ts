39e643dfc11e5eba6c2cf62781a33396dfbf22bd
Tito Moi <moidmedina@gmail.com>
2024-03-19 14:14:57 +0100

    wip: working on defer

---
+++ src/app/statistics/fake-accordion/fake-accordion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+import { MatExpansionModule } from "@angular/material/expansion";
+import { TranslocoDirective } from "@ngneat/transloco";
+
+@Component({
+  selector: "app-fake-accordion",
+  standalone: true,
+  imports: [MatExpansionModule, TranslocoDirective],
+  templateUrl: "./fake-accordion.component.html",
+  styleUrl: "./fake-accordion.component.scss",
+})
+export class FakeAccordionComponent {}
