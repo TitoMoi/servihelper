612dea6d7ca7aa61b7b52993a4983659efc76e21
Moisés Medina <moidmedina@gmail.com>
2024-06-09 22:20:31 +0200

    wip: separated columns

--- src/app/map/data-ready-s13/data-ready-s13.component.ts
+++ src/app/map/data-ready-s13/data-ready-s13.component.ts
@@ -1,16 +1,18 @@
 import { Component } from "@angular/core";
 import { TerritoryService } from "../territory/service/territory.service";
 import { ParticipantService } from "app/participant/service/participant.service";
+import { ParticipantPipe } from "app/participant/pipe/participant.pipe";
+import { TranslocoDatePipe } from "@ngneat/transloco-locale";
 
 @Component({
   selector: "app-data-ready-s13",
   standalone: true,
-  imports: [],
+  imports: [ParticipantPipe, TranslocoDatePipe],
   templateUrl: "./data-ready-s13.component.html",
   styleUrl: "./data-ready-s13.component.scss",
 })
 export class DataReadyS13Component {
-  territories = this.territoryService.getTerritories();
+  territories = this.territoryService.getTerritories().filter((t) => t.available);
   participants = this.participantService.getParticipants();
 
   constructor(
