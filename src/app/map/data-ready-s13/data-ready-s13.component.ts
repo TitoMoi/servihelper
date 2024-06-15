abaa2f577d70b9e05cb2da999b4785dc31b70f40
Moisés Medina <moidmedina@gmail.com>
2024-06-15 18:30:56 +0200

    wip: working s13 data ready

--- src/app/map/data-ready-s13/data-ready-s13.component.ts
+++ src/app/map/data-ready-s13/data-ready-s13.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TerritoryService } from "../territory/service/territory.service";
 import { ParticipantService } from "app/participant/service/participant.service";
 import { ParticipantPipe } from "app/participant/pipe/participant.pipe";
@@ -11,12 +11,22 @@ import { TranslocoDatePipe } from "@ngneat/transloco-locale";
   templateUrl: "./data-ready-s13.component.html",
   styleUrl: "./data-ready-s13.component.scss",
 })
-export class DataReadyS13Component {
-  territories = this.territoryService.getTerritories().filter((t) => t.available);
+export class DataReadyS13Component implements OnInit {
+  chunkSize = 20;
+
+  territoriesSource = this.territoryService.getTerritories().filter((t) => t.available);
+  territoriesChunks = [];
+
   participants = this.participantService.getParticipants();
 
   constructor(
     private territoryService: TerritoryService,
     private participantService: ParticipantService,
   ) {}
+  ngOnInit(): void {
+    for (let i = 0; i < this.territoriesSource.length; i += this.chunkSize) {
+      const chunk = this.territoriesSource.slice(i, i + this.chunkSize);
+      this.territoriesChunks.push(chunk);
+    }
+  }
 }
